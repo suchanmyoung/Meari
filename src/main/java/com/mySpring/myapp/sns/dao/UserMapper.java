0aeb9a68085eb97b2878d15a3ff63fa34df16172
suchan <aud248@naver.com>
2022-04-07 16:17:47 +0900

    test: db insert 테스트

--- src/main/java/com/mySpring/myapp/sns/dao/UserMapper.java
+++ src/main/java/com/mySpring/myapp/sns/dao/UserMapper.java
@@ -3,11 +3,11 @@ package com.mySpring.myapp.sns.dao;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
-@Repository
 @Mapper
 public interface UserMapper {
-    @Insert("INSERT INTO testdb(test_data, test_dates) values (#{name}, #{age})")
-    void save(@Param("name") String name, @Param("age") String age);
+    @Select("SELECT COUNT(*) FROM testdb")
+    int save(@Param("name") String name, @Param("age") String age);
 }
