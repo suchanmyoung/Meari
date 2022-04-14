a582884919f531ea73eb4bd5acb1bfdecfef1648
suchan <aud248@naver.com>
2022-04-14 12:29:43 +0900

    refactor : MemberDAO Interface 삭제

--- src/main/java/com/mySpring/myapp/member/dao/MemberDAO.java
+++ src/main/java/com/mySpring/myapp/member/dao/MemberDAO.java
@@ -3,19 +3,46 @@ package com.mySpring.myapp.member.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
 
 import com.mySpring.myapp.member.vo.MemberVO;
+import com.mySpring.myapp.member.vo.UserProfileVO;
+import com.mySpring.myapp.sns.vo.ImageVO;
 
-public interface MemberDAO {
-	 //public List selectAllMemberList() throws DataAccessException;
-	 public int insertMember(MemberVO memberVO) throws DataAccessException ;
-	 //public int deleteMember(String member_id) throws DataAccessException;
-	 public MemberVO loginById(MemberVO memberVO) throws DataAccessException;
-	 public List newImageList(String member_id) throws DataAccessException;
-	 
-	 public List userProfile(String member_id) throws DataAccessException;
-     
-	    public int userProfileUpdate(Map articleMap) throws DataAccessException;
-     
+
+@Repository("memberDAO")
+public class MemberDAO {
+    @Autowired
+    private SqlSession sqlSession;
+
+
+    public int userProfileUpdate(Map articleMap) throws DataAccessException {
+        int articleNO = sqlSession.insert("mapper.member.userProfileUpdate", articleMap);
+        return articleNO;
+    }
+
+    public List userProfile(String member_id) throws DataAccessException {
+        List<UserProfileVO> userProfile = sqlSession.selectList("mapper.member.userProfile", member_id);
+        return userProfile;
+    }
+
+    public int insertMember(MemberVO memberVO) throws DataAccessException {
+        int result = sqlSession.insert("mapper.member.insertMember", memberVO);
+        return result;
+    }
+
+    public MemberVO loginById(MemberVO memberVO) throws DataAccessException {
+        MemberVO vo = sqlSession.selectOne("mapper.member.loginById", memberVO);
+        return vo;
+    }
+
+    public List newImageList(String member_id) throws DataAccessException {
+        List<ImageVO> sns_newImageList = sqlSession.selectList("mapper.sns.profileFileList", member_id);
+        return sns_newImageList;
+    }
+
+
 }
