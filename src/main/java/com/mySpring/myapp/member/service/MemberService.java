a2e3551692f3fe028b4bca591ac17408219884c3
suchan <aud248@naver.com>
2022-04-14 12:28:43 +0900

    refactor : MemberService Interface 삭제

--- src/main/java/com/mySpring/myapp/member/service/MemberService.java
+++ src/main/java/com/mySpring/myapp/member/service/MemberService.java
@@ -3,18 +3,46 @@ package com.mySpring.myapp.member.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mySpring.myapp.member.dao.MemberDAO;
 import com.mySpring.myapp.member.vo.MemberVO;
 import com.mySpring.myapp.member.vo.UserProfileVO;
 import com.mySpring.myapp.sns.vo.ImageVO;
 
-public interface MemberService {
-	 //public List listMembers() throws DataAccessException;
-	 public int addMember(MemberVO memberVO) throws DataAccessException;
-	 //public int removeMember(String member_id) throws DataAccessException;
-	 public MemberVO login(MemberVO memberVO) throws Exception;
-	 public List<ImageVO> newImageList(String member_id) throws Exception;
-	 public int userProfileUpdate(Map articleMap) throws Exception;
-     public List<UserProfileVO> userProfile(String member_id) throws Exception;
+
+@Service("memberService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class MemberService {
+    @Autowired
+    private MemberDAO memberDAO;
+
+
+    public int userProfileUpdate(Map articleMap) throws Exception {
+        int articleNO = memberDAO.userProfileUpdate(articleMap);
+        return articleNO;
+    }
+
+    public List<UserProfileVO> userProfile(String member_id) throws Exception {
+        List<UserProfileVO> userProfile = memberDAO.userProfile(member_id);
+        return userProfile;
+    }
+
+    public int addMember(MemberVO member) throws DataAccessException {
+        return memberDAO.insertMember(member);
+    }
+
+    public MemberVO login(MemberVO memberVO) throws Exception {
+        return memberDAO.loginById(memberVO);
+    }
+
+    public List<ImageVO> newImageList(String member_id) throws Exception {
+        List<ImageVO> sns_newImageList = memberDAO.newImageList(member_id);
+        return sns_newImageList;
+    }
+
 }
